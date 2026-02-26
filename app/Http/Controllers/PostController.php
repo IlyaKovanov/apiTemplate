43ccd4888fe885b9fbd8c4c2f61bc336639f4c0a
IlyaKovanov <i.kovanov@yandex.ru>
2026-02-26 20:14:53 +0300

    create PostRequest & validate data to controller

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,12 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\PostRequest;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     public function index()
     {
-        return 'test index';
+
+        return 'success';
+    }
+
+    public function store(PostRequest $request)
+    {
+        $validated = $request->validated();
+
+        return response()->json([
+            'message' => 'Пост успешно создан',
+            'data' => $validated
+        ]);
     }
 }

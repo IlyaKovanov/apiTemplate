cd0787ea13ec2ccbe1cbd3913001342ff6930a80
IlyaKovanov <i.kovanov@yandex.ru>
2026-02-26 18:13:31 +0300

    create Category model & migration, add SoftDeletes to Post model

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,18 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+//Route::get('/user', function (Request $request) {
+//    return $request->user();
+//})->middleware('auth:sanctum');
+
+Route::controller(PostController::class)->group(function () {
+    Route::get('/posts', 'index');
+    Route::post('/posts', 'store');
+    Route::get('/posts/{id}', 'show');
+    Route::put('/posts/{id}', 'update');
+    Route::delete('/posts/{id}', 'destroy');
+});
+
